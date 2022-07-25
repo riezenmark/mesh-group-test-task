b356aa13c332e3adf6fbccebe21c6cf21d595e15
riezenmark <riezenmark@gmail.com>
2022-07-26 03:09:50 +1000

    Create: project structure

---
+++ src/main/java/ru/meshgroup/testtask/MeshGroupTestTaskApplication.java
@@ -0,0 +1,13 @@
+package ru.meshgroup.testtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeshGroupTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeshGroupTestTaskApplication.class, args);
+    }
+
+}
